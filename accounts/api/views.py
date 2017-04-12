e64224f8ea3c71a209644aea109ada807c8ccf8d
Yura <cherniakhov@gmail.com>
2017-04-12 15:20:24 +0300

    before first makemigrations and before migrate

---
+++ accounts/api/views.py
@@ -0,0 +1,37 @@
+from rest_framework.generics import (
+    CreateAPIView,
+    DestroyAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    RetrieveUpdateAPIView
+    )
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAdminUser,
+    IsAuthenticatedOrReadOnly,
+    )
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+#from posts.api.permissions import IsOwnerOrReadOnly
+#from posts.api.pagination import PostLimitOffsetPagination, PostPageNumberPagination
+
+from .serializers import (
+    UserCreateSerializer
+    )
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserCreateSerializer
+    queryset = User.objects.all()
+
+
+class UserLoginAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serializer = UserLoginSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            new_data = serializer.data
+            return Response(new_data, status=HTTP_200_OK)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
---
+++ accounts/models.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+# class User(models.Model):
+#     email = models.CharField(max_length=255)
+#     ﬁrst_name = models.CharField(max_length=255)
+#     last_name = models.CharField(max_length=255)
+#     phone_number = models.CharField(max_length=255)
+#     address = models.CharField(max_length=255)
+#
+#     name = models.CharField(max_length=255)
+#     description = models.TextField()
+#     price = models.DecimalField(decimal_places=2, max_digits=20)
+#
+#     email, ﬁrst_name, last_name, phone_number, address
